580a18257a2926004836def0ce37cf365435617b
rmg1002 <rmg1002@gmail.com>
2020-08-11 15:28:46 +0900

    board1

---
+++ map/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Point(models.Model):
+    title = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+# Create your models here.
